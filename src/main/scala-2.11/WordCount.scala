b45f72cfb3efd2155eaf0d7928d5549999520625
Aaron Yang <ycr_aaron@foxmail.com>
2016-09-16 01:30:52 +0800

    Initial commit

---
+++ src/main/scala-2.11/WordCount.scala
@@ -0,0 +1,21 @@
+/**
+  * Created by Aaron on 16/9/16.
+  */
+
+import org.apache.spark.{SparkContext, SparkConf}
+
+object WordCount {
+  def main (args: Array[String]): Unit ={
+    val conf = new SparkConf()
+      .setMaster("local[*]")
+      .setAppName("Spark IDEA Test")
+      .set("spark.executor.memory", "2g")
+
+    val sc = new SparkContext(conf)
+    val lines = sc.parallelize(Seq("First mo jie line", "Second ying line", "Third yang cheng rui jie line"))
+    val counts = lines.flatMap(line=>line.split(" "))
+      .map(word=>(word,1))
+      .reduceByKey(_+_)
+    counts.foreach(println)
+  }
+}
